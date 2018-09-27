3a857a906da099938fff75ffbdfc527191b3bb9f
JCM <joaquim_castro_moura@hotmail.com>
2018-09-27 13:21:37 -0300

    Consulta simple com @Query.

--- src/main/java/br/com/cursoudemy/repository/PostRepository.java
+++ src/main/java/br/com/cursoudemy/repository/PostRepository.java
@@ -3,12 +3,18 @@ package br.com.cursoudemy.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.cursoudemy.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	// campo pelo qual a busca sera realizada ='title'
+	
+	@Query("{ 'title' : { $regex: ?0, $options: 'i'} }")
+	List<Post> findByTitle(String text);
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
