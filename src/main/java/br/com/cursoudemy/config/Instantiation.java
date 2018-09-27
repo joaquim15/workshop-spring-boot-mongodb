28e9f647640fc6aa9d861a36e0b462533c28ed1d
JCM <joaquim_castro_moura@hotmail.com>
2018-09-27 10:14:00 -0300

    Referenciando ps posts do usuario.

--- src/main/java/br/com/cursoudemy/config/Instantiation.java
+++ src/main/java/br/com/cursoudemy/config/Instantiation.java
@@ -38,12 +38,14 @@ public class Instantiation implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços",
-				new AuthorDTO(maria));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei Feliz hoje.", new AuthorDTO(maria));
 
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getListPosts().addAll(Arrays.asList(post1, post2));
 
+		userRepository.save(maria);
 	}
 
 }
--- src/main/java/br/com/cursoudemy/domain/User.java
+++ src/main/java/br/com/cursoudemy/domain/User.java
@@ -1,19 +1,25 @@
 package br.com.cursoudemy.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = -5741638483187091286L;
-	
+
 	@Id
 	private String id;
 	private String name;
 	private String email;
+
+	@DBRef(lazy = true)
+	private List<Post> listPosts = new ArrayList<>();
 
 	public User() {
 		// TODO Auto-generated constructor stub
@@ -50,6 +56,14 @@ public class User implements Serializable {
 		this.email = email;
 	}
 
+	public List<Post> getListPosts() {
+		return listPosts;
+	}
+
+	public void setListPosts(List<Post> listPosts) {
+		this.listPosts = listPosts;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -74,5 +88,4 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
-
 }
